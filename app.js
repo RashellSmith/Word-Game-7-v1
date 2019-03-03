7b756e253c1d46b15b71f0062aee2f63c37ffe09
Rashsmit <33126899+Rashsmit@users.noreply.github.com>
2019-03-03 12:08:08 -0500

    update to app.js

--- app.js
+++ app.js
@@ -3,26 +3,26 @@ const phrase = document.getElementById('phrase');
 let missed = 0;
 let start= document.querySelector('a');
 let phrases = ["crazy", "uneventful", "coding","javascript","python"];
+let ul =document.getElementById("phrase")
 
  start.addEventListener("click", (e) => {
   document.getElementById("overlay").style.visibility = "hidden";
-  function getRandomPhraseAsArray(arr){
-    let newPhrase = phrases[Math.floor(Math.random()*phrases.length)];
-    let phraseGuess = newPhrase.split("");
-    // console.log(phraseGuess);
-  }
-  // getRandomPhraseAsArray(phrases);
-
-  function addPhraseToDisplay(arr){
-    for (i = 0; i < phrases.length; i++) {
-      var node = document.createElement("LI");                 // Create a <li> node
-      var textnode = document.createTextNode(phrases[i]);         // Create a text node
-      node.appendChild(textnode);                              // Append the text to <li>
-      document.getElementById("phrase").appendChild(node);
-}
-const phraseArray = getRandomPhraseAsArray(phrases);
-addPhrasetoDisplay(phraseArray);
-    // do stuff any arr that is passed in, and add to `#phrase ul`
-}
-
 });
+
+
+const getRandomPhraseAsArray= arr =>{
+  let newPhrase = phrases[Math.floor(Math.random()*phrases.length)];
+  let phraseGuess = newPhrase.split("");
+}
+
+const addPhraseToDisplay= arr =>{
+  for (let i = 0; i < arr.length; i += 1) {
+    let li = document.createElement("li");                 // Create a <li> node
+    let textnode = document.createTextNode(arr[i]);         // Create a text node
+    li.appendChild(textnode);                              // Append the text to <li>
+    ul.appendChild(li);
+}
+};
+
+const phraseArray = getRandomPhraseAsArray(phrases);
+addPhraseToDisplay(phraseArray);
