e3f2bd2597e120e319fbfb295f8fae46ebbba0de
Rashsmit <33126899+Rashsmit@users.noreply.github.com>
2019-03-06 22:16:29 -0500

    checkLetter function.

--- app.js
+++ app.js
@@ -13,16 +13,31 @@ let ul =document.getElementById("phrase")
 const getRandomPhraseAsArray= arr =>{
   let newPhrase = phrases[Math.floor(Math.random()*phrases.length)];
   let phraseGuess = newPhrase.split("");
+  return phraseGuess;
 }
 
 const addPhraseToDisplay= arr =>{
   for (let i = 0; i < arr.length; i += 1) {
     let li = document.createElement("li");                 // Create a <li> node
     let textnode = document.createTextNode(arr[i]);         // Create a text node
+    if ( arr[i]!= ""){
     li.appendChild(textnode);                              // Append the text to <li>
-    ul.appendChild(li);
-}
+    ul.appendChild(li).classList.add("letter");
+  }
 };
-
+};
 const phraseArray = getRandomPhraseAsArray(phrases);
 addPhraseToDisplay(phraseArray);
+
+
+const elementsArray = document.querySelectorAll("button");
+
+ checkLetter.forEach(function(elem) {
+    elem.addEventListener('click', (e)=> {
+      const letters = document.getElementsByClassName('letter');
+      if (e.innerHTML == leters[i]){
+        e.
+      }
+                   //this function does stuff
+               });
+           })
